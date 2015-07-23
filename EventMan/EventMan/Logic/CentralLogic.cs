d52b778c3091e541aff119f933022cdd4f25f3df
b49tan <b49tan@gmail.com>
2015-07-23 20:55:48 +0530

    new changes

--- EventMan/EventMan/Logic/CentralLogic.cs
+++ EventMan/EventMan/Logic/CentralLogic.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Web;
 
@@ -11,36 +12,43 @@ namespace EventMan.Logic
 
         public IEnumerable<EventMan.Event> GetEvent()
         {
-            var evt = from even in db.Events
-                      select even;
-            return evt;
+            //var evt = from even in db.Events
+            //          select even;
+            var evt = db.Events.AsEnumerable();
+            return evt;            
         }
 
         public IQueryable<EventMan.Event> GetEvent(int Id)
         {
-            var evt = from even in db.Events
-                      where even.Id == Id
-                      select even;
+            //var evt = from even in db.Events
+              //        where even.Id == Id
+                //      select even;
+            var evt = db.Events.Where( e => e.Id.Equals(Id));
             return evt;
         }
         public IEnumerable<EventMan.StakeHolder> GetStakeHolder()
         {
-            var evt = from even in db.StakeHolders
-                      select even;
+            //var evt = from even in db.StakeHolders
+              //        select even;
+            var evt = db.StakeHolders.AsEnumerable();
             return evt;
         }
         public IQueryable<EventMan.StakeHolder> GetStakeHolder(int Id)
         {
-            var evt = from even in db.StakeHolders
-                      where even.Id == Id
-                      select even;
+            //var evt = from even in db.StakeHolders
+              //        where even.Id == Id
+                //      select even;
+
+            var evt = db.StakeHolders.Where(e => e.Id.Equals(Id));
             return evt;
         }
         public IEnumerable<EventMan.StakeHolder> GetStakeHolderByType(int type)
         {
-            var evt = from even in db.StakeHolders
-                      where even.Type == type
-                      select even;
+            //var evt = from even in db.StakeHolders
+              //        where even.Type == type
+                //      select even;
+
+            var evt = db.StakeHolders.Where(e => e.Type.Equals(type));
             return evt;
         }
         internal IQueryable<StakeHolder> GetStakeHolderByLogin(string username, string password, long? mobile, string email)
@@ -52,33 +60,45 @@ namespace EventMan.Logic
             string pass = password.Trim().ToLower();
             string usern = username.Trim().ToLower();
             string emailr = email.Trim().ToLower();
-            var stakeholder = from guy in db.StakeHolders
-                              where guy.Password.Trim().ToLower() == (pass) &&
-                                    (guy.Username.Trim().ToLower() == (usern) ||
+            //var stakeholder = from guy in db.StakeHolders
+              //                where guy.Password.Trim().ToLower() == (pass) &&
+                //                    (guy.Username.Trim().ToLower() == (usern) ||
+                  //                  guy.Email.Trim().ToLower() == (emailr) ||
+                     //               guy.Mobile == (mobile))
+                       //       select guy;
+
+            var stakeholder = db.StakeHolders.Where(guy => 
+                (guy.Password.Trim().ToLower() == (pass) &&
+                (guy.Username.Trim().ToLower() == (usern) ||
                                     guy.Email.Trim().ToLower() == (emailr) ||
-                                    guy.Mobile == (mobile))
-                              select guy;
+                                    guy.Mobile == (mobile)) ));
             return stakeholder;
         }
-        public IQueryable<EventMan.Registration> GetRegistration()
+        public IEnumerable<EventMan.Registration> GetRegistration()
         {
-            var evt = from even in db.Registrations
-                      select even;
+            //var evt = from even in db.Registrations
+              //        select even;
+            var evt = db.Registrations.AsEnumerable();
             return evt;
         }
 
         public IEnumerable<int> GetEventRegistrationByStakeHolderId(int Id)
         {
-            var evt = from even in db.Registrations
-                      where even.StakeHolderId == Id
-                      select even.EventId;
+            //var evt = from even in db.Registrations
+              //        where even.StakeHolderId == Id
+                //      select even.EventId;
+
+            var evt = db.Registrations.Where(e => e.StakeHolderId.Equals(Id)).Select( e=> e.EventId);
+        
             return evt;
         }
         public IQueryable<EventMan.Registration> GetRegistrationByEventId(int Id)
         {
-            var evt = from even in db.Registrations
-                      where even.EventId == Id
-                      select even;
+            //var evt = from even in db.Registrations
+              //        where even.EventId == Id
+                //      select even;
+            var evt = db.Registrations.Where(e => e.EventId.Equals(Id));
+        
             return evt;
         }
 
@@ -92,8 +112,9 @@ namespace EventMan.Logic
 
         public IEnumerable<Sponsor> GetSponsor()
         {
-            var spnsr = from spns in db.Sponsors
-                        select spns;
+            //var spnsr = from spns in db.Sponsors
+              //          select spns;
+            var spnsr = db.Sponsors.AsEnumerable();
             return spnsr;
         }
 
